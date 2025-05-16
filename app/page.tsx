d6d258517e86a892d040021eecbe5b6c95ae3e47
Wiem Mimouni <wiemmimouni74@gmail.com>
2025-05-16 16:40:20 -0400

    hello

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Hero from '@/components/Hero';
-import Steps from '@/components/Steps';
+/* import Steps from '@/components/Steps';
 import Features from '@/components/Features';
 import CTA from '@/components/CTA';
-import Footer from '@/components/Footer';
+import Footer from '@/components/Footer'; */
 
 export default function Home() {
   return (
